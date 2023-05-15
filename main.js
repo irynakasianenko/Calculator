c88351758e1d3eb622b7aeffb1db3beadd373b8f
Iryna Kasianenko <iryna.kasianenko98@gmail.com>
2023-05-15 13:40:54 +0300

    add function getExpression

--- main.js
+++ main.js
@@ -1,40 +1,33 @@
-function calculate() {
-	let buttonsNumbers = document.querySelector('.buttons-numbers').children;
-	let operands = Array.from(buttonsNumbers);
-	let display = document.querySelector('#display');
-	let clear = document.querySelector('#clear');
-	let equals = document.querySelector('#equals');
-	let buttonsOperators = document.querySelector('.buttons-operators').children;
-	let operators = Array.from(buttonsOperators);
+let buttonsNumbers = document.querySelector('.buttons-numbers').children;
+let operands = Array.from(buttonsNumbers);
+let display = document.querySelector('#display');
+let clear = document.querySelector('#clear');
+let equals = document.querySelector('#equals');
+let buttonsOperators = document.querySelector('.buttons-operators').children;
+let operators = Array.from(buttonsOperators);
 
-
-	operands.forEach((btn) => {
-		btn.addEventListener('click', function () {
-			display.innerText += btn.value;
-			// if (display.innerText.startsWith('0')) {
-			// 	display.innerText.replace(/^0+/, '');
-			// }
-		})
+operands.forEach((btn) => {
+	btn.addEventListener('click', function () {
+		display.innerText += btn.value;
 	})
+})
 
-	operators.forEach((btn) => {
-		btn.addEventListener('click', function () {
-			display.innerText += btn.value;
-		})
+operators.forEach((btn) => {
+	btn.addEventListener('click', function () {
+		display.innerText += btn.value;
 	})
+})
 
+clear.addEventListener('click', function () {
+	display.innerText = '';
+})
 
-	clear.addEventListener('click', function () {
-		display.innerText = '';
-	})
+equals.addEventListener('click', function () {
+	let result = getExpression(display.innerText);
+	display.innerText = result;
+})
 
-
-
-	equals.addEventListener('click', function () {
-		let result = eval(display.innerText);
-		display.innerText = result;
-	})
-
+function getExpression(expression) {
+	let func = new Function("return " + expression);
+	return func();
 }
-
-calculate();
