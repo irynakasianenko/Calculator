3536a5a335298b7705a4d0b03edf4f408e6ff1bf
Iryna Kasianenko <iryna.kasianenko98@gmail.com>
2023-05-19 15:16:32 +0300

    add function for complex calculations

--- main.js
+++ main.js
@@ -10,31 +10,58 @@ buttons.addEventListener('click', calculate);
 
 function calculate(e) {
 	if (e.target.parentElement.classList.contains('buttons-numbers')) {
-		display.innerText += e.target.value;
-		operator += e.target.value;
+		getOperator(e);
 	}
 	if (e.target.parentElement.classList.contains('buttons-operators')) {
+		getOperand(e);
+	}
+	if (e.target.classList.contains('equals')) {
+		getEquality();
+	}
+	if (e.target.classList.contains('clear')) {
+		clear();
+	}
+	if (e.target.classList.contains('delete')) {
+		deleteLast();
+	}
+}
+
+function getOperator(e) {
+	display.innerText += e.target.value;
+	operator += e.target.value;
+}
+
+function getOperand(e) {
+	if (display.innerText.indexOf('+') > 0 || display.innerText.indexOf('-') > 0 || display.innerText.indexOf('*') > 0 || display.innerText.indexOf('/') > 0) {
+		operators.push(Number(operator));
+		getResult(operators, operand);
+		operators = [];
+		operators.push(result);
+		operator = '';
+		operand = e.target.value;
+		display.innerText += operand;
+	} else {
 		display.innerText += e.target.value;
 		operand += e.target.value;
 		operators.push(Number(operator));
 		operator = '';
 	}
-	if (e.target.classList.contains('equals')) {
-		getResult(operators, operand);
-		operators = [];
-		operator = result;
-		operand = '';
-	}
-	if (e.target.classList.contains('clear')) {
-		display.innerText = '';
-		operators = [];
-		operand = '';
-		result = '';
-		operator = '';
-	}
-	if (e.target.classList.contains('delete')) {
-		deleteLast();
-	}
+}
+
+function getEquality() {
+	operators.push(Number(operator));
+	getResult(operators, operand);
+	operators = [];
+	operator = result;
+	operand = '';
+}
+
+function clear() {
+	display.innerText = '';
+	operators = [];
+	operand = '';
+	result = '';
+	operator = '';
 }
 
 function getResult(operators, operand) {
@@ -52,34 +79,33 @@ function getResult(operators, operand) {
 			result = operators[0] / operators[1];
 			break;
 	}
+	if (!Number.isInteger(result)) {
+		result = result.toFixed(4);
+	}
 	display.innerText = result;
 	return result;
 }
 
+function deleteLast() {
+	display.innerText = display.innerText.slice(0, -1);
+	operator = display.innerText;
+}
+
 document.addEventListener('keydown', function (e) {
-	if (!isNaN(Number(e.key))) {
-		display.innerText += e.key;
-		operator += e.key;
+	e.preventDefault();
+	if (!isNaN(Number(e.key)) || e.key == '.') {
+		e.target.value = e.key;
+		getOperator(e);
 	}
 	if (e.key == '+' || e.key == '-' || e.key == '*' || e.key == '/') {
-		display.innerText += e.key;
-		operand += e.key;
-		operators.push(Number(operator));
-		operator = '';
+		e.target.value = e.key;
+		getOperand(e)
 	}
-	if (e.key == '=') {
-		operators.push(Number(operator));
-		getResult(operators, operand);
-		operators = [];
-		operator = result;
-		operand = '';
+	if (e.key == '=' || e.key == 'Enter') {
+		getEquality();
 	}
 	if (e.key == 'Backspace') {
 		deleteLast();
 	}
 })
 
-function deleteLast() {
-	display.innerText = display.innerText.slice(0, -1);
-	operator = operator.slice(0, -1);
-}
